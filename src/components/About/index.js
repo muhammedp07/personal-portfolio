3abd39f1554dd8f2382c5e8127bf9aaf974056df
p47n <patelmuhammed7777@gmail.com>
2024-09-05 17:41:39 -0230

    changed font size in about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -35,16 +35,16 @@ const About = () => {
               idx={15}
             />
           </h1>
-          <p>
+          <p style={{ fontSize: '17px', textAlign: 'left' }}>
             I'm a very ambitious front-end developer looking for a role in an
             established IT company with the opportunity to work with the latest
             technologies on challenging and diverse projects.
           </p>
-          <p align="LEFT">
+        <p style={{ fontSize: '17px', textAlign: 'left' }}> 
             I'm quiet confident, naturally curious, and perpetually working on
             improving my chops one design problem at a time.
           </p>
-          <p>
+          <p style={{ fontSize: '17px', textAlign: 'left' }}>
             If I need to define myself in one sentence that would be a family
             person, father of a beautiful daughter, a sports fanatic,
             photography enthusiast, and tech-obsessed!!!
