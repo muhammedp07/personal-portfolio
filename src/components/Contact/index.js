a7adaa064006dfaea343b2ccf65a3a5aacda319b
p47n <patelmuhammed7777@gmail.com>
2024-09-06 01:43:45 -0230

    added socials on contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,10 @@
 import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
-import { useRef } from 'react'
-import emailjs from '@emailjs/browser'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const form = useRef()
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -17,21 +14,6 @@ const Contact = () => {
     // Cleanup function to clear the timeout if the component unmounts
     return () => clearTimeout(timer);
   }, []);
-  const sendEmail = (e) => {
-    e.preventDefault()
-
-    emailjs
-      .sendForm('gmail', 'template_YeJhZkgb', form.current, 'your-token')
-      .then(
-        () => {
-          alert('Message successfully sent!')
-          window.location.reload(false)
-        },
-        () => {
-          alert('Failed to send the message, please try again')
-        }
-      )
-  }
 
   return (
     <>
@@ -44,45 +26,18 @@ const Contact = () => {
               idx={15}
             />
           </h1>
-          <p>
-            I am interested in freelance opportunities - especially on ambitious
-            or large projects. However, if you have any other requests or
-            questions, don't hesitate to contact me using below form either.
+          <p align='LEFT'>
+            I Am always up for a tech adventure or a good chat about anything geeky. 
+            Got a burning question or just want to swap memes? 
+            Hit me up through the links below—let's make some magic happen!
           </p>
-          <div className="contact-form">
-            <form ref={form} onSubmit={sendEmail}>
-              <ul>
-                <li className="half">
-                  <input placeholder="Name" type="text" name="name" required />
-                </li>
-                <li className="half">
-                  <input
-                    placeholder="Email"
-                    type="email"
-                    name="email"
-                    required
-                  />
-                </li>
-                <li>
-                  <input
-                    placeholder="Subject"
-                    type="text"
-                    name="subject"
-                    required
-                  />
-                </li>
-                <li>
-                  <textarea
-                    placeholder="Message"
-                    name="message"
-                    required
-                  ></textarea>
-                </li>
-                <li>
-                  <input type="submit" className="flat-button" value="SEND" />
-                </li>
-              </ul>
-            </form>
+          <div className="contact-info">
+            <p>
+              LinkedIn: <a href="https://www.linkedin.com/in/muhammedpatel007/" target="_blank" rel="noopener noreferrer">Connect on LinkedIn</a>
+            </p>
+            <p>
+              Discord: <a href="https://discord.com/users/pa1n_.007" target="_blank" rel="noopener noreferrer">Let's chat and geek out!</a>
+            </p>
           </div>
         </div>
       </div>
